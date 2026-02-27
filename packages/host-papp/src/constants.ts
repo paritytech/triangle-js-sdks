8d25c2d7203944f74fc75d51781818b6b0be04b0
Tanya Gudyma <tanyagta@bk.ru>
2026-02-27 12:08:10 +0300

    feat: update stable stage endpoints

--- packages/host-papp/src/constants.ts
+++ packages/host-papp/src/constants.ts
@@ -1,6 +1,3 @@
 export const SS_UNSTABLE_STAGE_ENDPOINTS = ['wss://pop-testnet.parity-lab.parity.io:443/9910'];
-export const SS_STABLE_STAGE_ENDPOINTS = [
-  'wss://pop3-testnet.parity-lab.parity.io:443/7911',
-  'wss://pop3-testnet.parity-lab.parity.io:443/7912',
-];
+export const SS_STABLE_STAGE_ENDPOINTS = ['wss://pop3-testnet.parity-lab.parity.io/people'];
 export const SS_PREVIEW_STAGE_ENDPOINTS = ['wss://previewnet.substrate.dev/people'];
