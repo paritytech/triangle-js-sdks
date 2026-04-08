9ee51592dae9b1de36a10ceb17672c012d15d72e
Sergey Zhuravlev <zhuravlev1337@gmail.com>
2026-04-08 12:59:46 +0200

    chore: optimized hostApi and container wrappers

--- packages/host-container/src/types.ts
+++ packages/host-container/src/types.ts
@@ -16,16 +16,17 @@ import { okAsync } from 'neverthrow';
 type SuccessResponse<T> = T extends { success: true; value: infer U } ? U : never;
 type ErrorResponse<T> = T extends { success: false; value: infer U } ? U : never;
 
-type Value<T extends Codec<any> | Codec<never>> = T extends Codec<any> ? CodecType<T> : unknown;
+export type CodecValue<T extends Codec<any> | Codec<never>> = T extends Codec<any> ? CodecType<T> : unknown;
 type OrPromise<T> = T | Promise<T>;
 type ExtractEnumValue<T> = T extends { tag: string; value: infer V } ? V : never;
-type WithVersion<V extends string, T> = ExtractEnumValue<Extract<T, { tag: V }>>;
 
-type UnwrapSuccessResponse<V extends string, T> = T extends { tag: infer Tag; value: infer Value }
+export type WithVersion<V extends string, T> = ExtractEnumValue<Extract<T, { tag: V }>>;
+
+export type UnwrapSuccessResponse<V extends string, T> = T extends { tag: infer Tag; value: infer Value }
   ? WithVersion<V, { tag: Tag; value: SuccessResponse<Value> }>
   : never;
 
-type UnwrapErrorResponse<V extends string, T> = T extends { tag: infer Tag; value: infer Value }
+export type UnwrapErrorResponse<V extends string, T> = T extends { tag: infer Tag; value: infer Value }
   ? WithVersion<V, { tag: Tag; value: ErrorResponse<Value> }>
   : never;
 
@@ -36,20 +37,22 @@ type UnwrapVersionedResult<V extends string, T> = T extends { tag: infer Tag; va
     >
   : never;
 
+export type ContainerRequestHandler<V extends string, T extends VersionedProtocolRequest> = (
+  params: WithVersion<V, CodecValue<T['request']>>,
+  helpers: {
+    ok: typeof okAsync<UnwrapSuccessResponse<V, CodecValue<T['response']>>>;
+    err: typeof errAsync<never, UnwrapErrorResponse<V, CodecValue<T['response']>>>;
+  },
+) => OrPromise<UnwrapVersionedResult<V, CodecValue<T['response']>>>;
+
 type InferRequestHandler<V extends string, T extends VersionedProtocolRequest> = (
-  callback: (
-    params: WithVersion<V, Value<T['request']>>,
-    helpers: {
-      ok: typeof okAsync<UnwrapSuccessResponse<V, Value<T['response']>>>;
-      err: typeof errAsync<never, UnwrapErrorResponse<V, Value<T['response']>>>;
-    },
-  ) => OrPromise<UnwrapVersionedResult<V, Value<T['response']>>>,
+  callback: ContainerRequestHandler<V, T>,
 ) => VoidFunction;
 
 type InferSubscribeHandler<V extends string, T extends VersionedProtocolSubscription> = (
   callback: (
-    params: WithVersion<V, Value<T['start']>>,
-    send: (payload: WithVersion<V, Value<T['receive']>>) => void,
+    params: WithVersion<V, CodecValue<T['start']>>,
+    send: (payload: WithVersion<V, CodecValue<T['receive']>>) => void,
     interrupt: VoidFunction,
   ) => VoidFunction,
 ) => VoidFunction;
